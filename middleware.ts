9187dc38d9e4588b6c14a8dc3618fd74245feef4
James Othniel <jamesothniel7@gmail.com>
2024-05-04 17:04:57 +0100

    update the middleware with the clerk api endpoint

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 // export default clerkMiddleware();
 
-export default authMiddleware({})
+export default authMiddleware({
+  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
